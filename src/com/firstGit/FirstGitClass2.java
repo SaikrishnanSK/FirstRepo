812e89d57b1c58f90792ba2819e4894ab2e9ac27
SaikrishnanSK <www.sai027@gmail.com>
2021-11-09 21:20:12 +0530

    updatedrepo

--- src/com/firstGit/FirstGitClass2.java
+++ src/com/firstGit/FirstGitClass2.java
@@ -1,8 +1,8 @@
 package com.firstGit;
 
-public class FirstGitClass {
+public class FirstGitClass2 {
 	public static void main(String[] args) {
-		System.out.println("class 1");
+		System.out.println("class 2");
 		
 		System.out.println("-----Success-----");
 	}
